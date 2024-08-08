8969c0aad24d6076c28852c75faba3c500d06dd4
noorulimaan1 <noor.imaan@arbisoft.com>
2024-08-08 13:37:03 +0500

    Revert "REFACTOR: Refactor code into modules"
    This reverts commit fbce708c0d07cbe0a6092fd0a6bef0a962a630b8.

--- file_reader.py
+++
@@ -1,8 +0,0 @@
-import os
-
-class FileReader:
-    @staticmethod
-    def get_files(files_dir: str, year: str):
-        for filename in os.listdir(files_dir):
-            if filename.endswith('.txt') and f"{year}_" in filename:
-                yield os.path.join(files_dir, filename)
